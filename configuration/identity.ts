e47623279dd45d678e96e3cd27b6a7116d475a4a
rpwwxc <ryan.pat.williams@gmail.com>
2025-03-01 18:36:39 -0500

    Description

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Ryan Williams`;
-export const OWNER_DESCRIPTION: string = `Joint Effects Planner`;
+export const OWNER_DESCRIPTION: string = `Ryan Williams is a Joint Effects Planner.  This means that he's the subject matter expert on all the U.S. Military's occupational specialties.  His job is to pay attention to current events, news articles discussing the US and it's interests, and then identify how the US can respond. Ryan needs to know how he can leverage U.S. Military personnel to respond to negative or positive news stories, or natural disasters, or humanitarian crisis, or armed conflict within the country of Bangladesh`;
 
 export const AI_NAME: string = `Minion`;
 export const AI_TONE: string = `Direct and in Active Voice`;
