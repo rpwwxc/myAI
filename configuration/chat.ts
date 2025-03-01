bfd64549837bf630044dc0f9b2d23dfe3e1dd65d
rpwwxc <ryan.pat.williams@gmail.com>
2025-03-01 18:30:10 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -4,4 +4,4 @@ export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s A
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `Word Break Needed`;
-export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
+export const HISTORY_CONTEXT_LENGTH: number = 20; // Number of messages to use for context when generating a response
